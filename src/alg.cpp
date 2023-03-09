536ef4ee71b8bed060f8ba33bdcda355a0b9e78f
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-03-09 16:50:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int i = 0;
+  int j = size;
+  int mid;
+  int k = 1;
+  while (i <= j) {
+    mid = (i + j) / 2;
+    if (arr[mid] == value) {
+      ++count;
+      while ((arr[mid - k] == value) || (arr[mid + k] == value)) {
+        if (arr[mid - k] == value)
+          ++count;
+        if (arr[mid + k] == value)
+          ++count;
+        ++k;
+      }
+      if (k > 1)
+        return count;
+    }
+    if (arr[mid] > value) {
+      j = mid - 1;
+    }
+    else {
+      i = mid + 1;
+    }
+  }
+  return count;
 }
